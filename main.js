c3681ba6b249181e4e625f29feb1dbb3e8d53d4b
shortykid <140464830+shortykid@users.noreply.github.com>
2024-04-12 19:32:38 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,18 @@
-function preload() {
+img="";
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
 }
-
 function setup(){
-    canvas = createCanvas(300, 300);
+    canvas = createCanvas(640, 420);
     canvas.center();
-    video = createCapture(VIDEO);
-    video.size(300,300);
-    video.hide();
-
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
 }
-function modelLoaded() {
-    console.log('poseNet Is Initialized')
-}
-
-function draw() {
-    image(video, 0 , 0 , 300 , 300);
-}
-
-function take_snapshot(){
-    save('myFilterImage.png');
-}
-
-function gotPoses(results) {
-
-    if(results.length > 0)
-    {
-        console.log(results);
-        console.log("nose x =" + results[0].pose.nose.x)
-        console.log("nose y =" + results[0].pose.nose.y)
-    }
+function draw()
+{
+image(img, 0, 0, 640, 420);
+fill("#FF0000")
+text("Dog", 45, 75);
+noFill();
+stroke("#FF0000")
+rect(30, 60, 450, 350);
 }
